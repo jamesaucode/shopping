f1571ba6fe60b0a9a06f12d1b98eb139a4746863
James Au <jamesaubudi@gmail.com>
2018-12-25 13:04:11 -0500

    Updates

---
+++ precache-manifest.155cb286c094f187eaf7105fe9883592.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4c43f3f98e252583d0a",
+    "url": "/shopping/static/js/runtime~main.c4c43f3f.js"
+  },
+  {
+    "revision": "1ae4af04d5efdbe2e1d0",
+    "url": "/shopping/static/js/main.1ae4af04.chunk.js"
+  },
+  {
+    "revision": "9a89aa5c764dc268882f",
+    "url": "/shopping/static/js/1.9a89aa5c.chunk.js"
+  },
+  {
+    "revision": "1ae4af04d5efdbe2e1d0",
+    "url": "/shopping/static/css/main.c333600e.chunk.css"
+  },
+  {
+    "revision": "7e3f4a29331ac258dbd1d26ad9c4d980",
+    "url": "/shopping/index.html"
+  }
+];
